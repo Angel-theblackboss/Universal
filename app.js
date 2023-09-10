a3b6cf567878ec9756ccad94a59c15fa5be5faf9
Angel-theblackboss <bntboris8@gmail.com>
2023-09-10 20:01:01 +0100

    azer commit

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const path = require('path');
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const clientRoutes = require('./router/client');
 
 
 
-mongoose.connect('mongodb+srv://Angel:Le24jan2022.@universal.btl7smq.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGODB_URL.toString(),
   { useNewUrlParser: true,
     useUnifiedTopology: true }) 
   .then(() => console.log('Connexion à MongoDB réussie !'))
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
+require('dotenv').config();
 
-server.listen(3000, () => {
-    console.log("Server is running at localhost:3000")
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running at localhost:${process.env.PORT}`)
 })
