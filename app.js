1ad229b8f85b06ae56100c892f0864ebbd4aea13
Angel-theblackboss <bntboris8@gmail.com>
2023-09-10 18:40:48 +0100

    Duiydfhjgyj commit

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ mongoose.connect('mongodb+srv://Angel:Le24jan2022.@universal.btl7smq.mongodb.net
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// Définir le dossier public où se trouve votre fichier index.html
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Gérer les requêtes à la racine en renvoyant le fichier index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.use(cors());
 
 app.use(express.json());//intercepte toutes les reexpress contenant du json et met son contenu à disposition 
